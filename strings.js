928962eb5669e1960c4b127e2084ad266c16e8a9
Muraya-Pat <patrickmuraya823@gmail.com>
2025-07-06 18:20:00 +0300

    Strings

---
+++ strings.js
@@ -0,0 +1,9 @@
+// string methods
+//creating a string
+const str = "Hello, World!";
+// Accessing characters
+console.log(str[0]); // Output: H
+console.log(str.charAt(1)); // Output: e
+// String length
+console.log(str.length); // Output: 13
+// String concatenation
