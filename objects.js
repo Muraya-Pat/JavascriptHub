e7dce1d30a9cb8b1480833a64bbdad20a957a711
Muraya-Pat <patrickmuraya823@gmail.com>
2025-07-06 18:10:59 +0300

    Updates on objects

--- objects.js
+++ objects.js
@@ -1,1 +1,25 @@
-// Objects in Javascrip
+// Objects in Javascript
+let person = {
+};
+// Adding properties to the object
+person.name = "John";
+person.age = 30;
+// Adding a method to the object
+person.greet = function() {
+    console.log("Hello, my name is " + this.name);
+};
+// Accessing properties
+console.log(person.name); // Output: John
+console.log(person.age); // Output: 30
+// Calling the method
+person.greet(); // Output: Hello, my name is John
+
+// an object that already has properties and values in it
+let car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020,
+    start: function() {
+        console.log("The car has started.");
+    }
+};
