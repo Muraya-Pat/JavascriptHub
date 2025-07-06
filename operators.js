30ed2fb072870eceecc31725cbec69d9fe04856d
Muraya-Pat <patrickmuraya823@gmail.com>
2025-07-06 15:09:18 +0300

    logical and comparison operators

--- operators.js
+++ operators.js
@@ -12,3 +12,25 @@ console.log("Difference:", difference);
 console.log("Product:", product);
 console.log("Quotient:", quotient);
 console.log("Remainder:", remainder);
+
+// comparison and logical operators
+let a = 10;
+let b = 20;
+let isEqual = a === b; // Strict equality
+let isNotEqual = a !== b; // Strict inequality
+let isGreater = a > b; // Greater than
+let isLess = a < b; // Less than
+let isGreaterOrEqual = a >= b; // Greater than or equal to
+let isLessOrEqual = a <= b; // Less than or equal to
+let andCondition = (a > 5) && (b < 30); // Logical
+let orCondition = (a < 5) || (b > 15); // Logical OR
+let notCondition = !(a === b); // Logical NOT
+console.log("Is Equal:", isEqual);
+console.log("Is Not Equal:", isNotEqual);
+console.log("Is Greater:", isGreater);
+console.log("Is Less:", isLess);
+console.log("Is Greater or Equal:", isGreaterOrEqual);
+console.log("Is Less or Equal:", isLessOrEqual);
+console.log("AND Condition:", andCondition);
+console.log("OR Condition:", orCondition);
+console.log("NOT Condition:", notCondition);
